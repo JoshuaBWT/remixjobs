a298ac0c61698494d1afaa7b0edccef7586e02db
JoshuaBWT <joshua.barette@hotmail.fr>
2015-12-13 00:37:39 +0100

    jobs scraping with /updateData + first api call /json done

---
+++ routes.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var path = require('path');
+var jobs = require('./lib/jobs.js');
+var scraper = require('./lib/scrap/remixScrap.js');
+
+module.exports = function(router)
+{
+  router.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+  });
+
+  // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+  router.get('/', function(req, res) {
+      res.json({ message: 'hooray! welcome to our api!' });
+  });
+
+  router.route('/jobs')
+    .post(function (req, res) {
+      //later
+      jobs.createOne(req, res);
+    })
+    .get(function (req, res) {
+       jobs.findAll(req, res);
+    });
+
+
+
+    //Fill the mongodb with some good scraping
+    router.route('/updateBase')
+      .get(function(req, res)
+      {
+          scraper.fetchAllJobs(req, res);
+      });
+}
---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var app = express();
+var router = express.Router();
+
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+app.use("/", router);
+
+mongoose.createConnection('mongodb://localhost:27017/remixjobs');
+
+//gestion des formulaires dans le module routes
+require("./routes")(router);
+
+//allumage du serveur
+var server = app.listen(3000, function () {
+  var port = server.address().port;
+
+  console.log('RemixJobs api listening at http://localhost:%s', port);
+});
